11b8f5a28586ca24a22b805d48e7414918867f62
papen12 <papenfabri2.0@gmail.com>
2026-04-11 21:13:12 -0400

    probar visualizacion

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st 
+import streamlit_shadcn_ui as ui
+
 
 st.write("Hola mundo")
 
 num1=st.number_input("numero 1")
+
+#titulo 
+
+st.title("Este es un titulo")
+
+st.header("Esta es un encabezado ")
+
+st.subheader("This is our Sub-header")
+
+st.caption("Este es un titulo pequeño")
+
+
+st.text("Hola mundo")
+
+st.markdown("# Título grande")
+st.markdown("## Subtítulo")
+st.markdown("### Más pequeño")
+st.markdown("***Texto en cursiva/negrita***")
+
+
+
+
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from proyecto!")
-
-
-if __name__ == "__main__":
-    main()
