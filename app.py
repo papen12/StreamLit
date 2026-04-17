6fdef2c2a4c7cce846907fc7cfe779432a366801
papen12 <papenfabri2.0@gmail.com>
2026-04-17 16:33:34 -0400

    Paginacion

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st 
 import streamlit_shadcn_ui as ui
-
-
+import pandas as pd
+import numpy as np 
+import altair as alt
 st.write("Hola mundo")
 
 num1=st.number_input("numero 1")
 
 #titulo 
+
+
 
 st.title("Este es un titulo")
 
@@ -23,6 +26,25 @@ st.markdown("# Título grande")
 st.markdown("## Subtítulo")
 st.markdown("### Más pequeño")
 st.markdown("***Texto en cursiva/negrita***")
+
+
+df = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['a', 'b']
+)
+
+
+chart = alt.Chart(df).mark_bar().encode(
+    x='a', y='b', tooltip=['a', 'b']
+)
+
+st.write(chart)
+
+
+"""
+# Magic Feature
+Markdown working without defining its function explicitly.
+"""
 
 
 
